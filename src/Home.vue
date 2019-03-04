91017fa63be5007a29db440c15c28e2f9cc76b94
muzi8 <muzi8@qq.com>
2019-03-04 11:20:54 +0800

    提交demo

---
+++ src/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app" :class="{'header-hide': headerHide}">
+    This is my {{ name }}
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      name: 'home'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
